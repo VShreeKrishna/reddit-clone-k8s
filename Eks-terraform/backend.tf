f67ef349ab79c52ace1e677cc798179bb5af7dd0
V Shreekrishna <137467113+VShreeKrishna@users.noreply.github.com>
2024-07-15 17:26:28 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "krishna-reddit777" # Replace with your actual S3 bucket name
+    bucket = "krishna-reddit77" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
